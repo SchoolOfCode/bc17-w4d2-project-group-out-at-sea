28637fcb4b12286a86ffd184742b9ff3e9441ec8
Elspeth <elspeth@mardaljevic.org>
2024-07-23 13:48:13 +0100

    user story 2 complete

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import helmet from 'helmet';
 const app = express();
 const port = 3000;
 
+// Ask Helmet to ignore the X-Powered-By header.
+app.use(
+    helmet({
+      xPoweredBy: false,
+    })
+  );
 app.get('/', (req, res) => {
     res.send('Hello World! ');
 });
@@ -10,3 +17,5 @@ app.listen(port, () => {
     console.log(`server started on ${port}`);
 });
 
+// Not required, but recommended for Express users:
+// app.disable("x-powered-by");
